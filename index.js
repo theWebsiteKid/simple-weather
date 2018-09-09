5e4254a8159ef474c606e72d8733b81830d619a7
Xavier Duncan <xduncan@pm.me>
2018-09-09 09:45:26 -0400

    fixed static file paths, housekeeping

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const path = require('path');
+const request = require('request');
 
-const server = express();
+const app = express();
 
-server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
-});
-
+// api request
+request('https://api.openweathermap.org/data/2.5/weather?zip=30305&appid=6aa6b5d216402a79cdb225a50c4ad9ab', 
+    (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            console.log('body:', body);
+        } else {
+            console.log('error', error);
+        }
+    }
+);
+// serve static files in public directory
+app.use(express.static('public'));
 // app running on port 3000
-server.listen(3000, () => {
-    console.log('SimpleWeather app running on port 3000')
+app.listen(3000, () => {
+    console.log('SimpleWeather app running on port 3000');
 });

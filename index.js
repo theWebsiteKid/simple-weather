048f08d59dfd40668c0ab5e708f1ecdd34961835
Xavier Duncan <xduncan@pm.me>
2018-09-09 10:50:52 -0400

    hide api key in .env file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const request = require('request');
 
 const app = express();
 
+// env variables
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+
 // api params
-let apiKey = '6aa6b5d216402a79cdb225a50c4ad9ab';
 let zip = '30305';
-let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${apiKey}`;
+let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${API_KEY}`;
 
 // api request
 request(url, (err, res, body) => {
@@ -19,9 +23,10 @@ request(url, (err, res, body) => {
 
     }
 });
+
 // serve static files in public directory
 app.use(express.static('public'));
 // app running on port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('SimpleWeather app running on port 3000');
 });
